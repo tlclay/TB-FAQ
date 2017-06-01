6d8e92b6f3e6735ea41d9b3fb29619b77a12c905
Novaras <toribashfear@gmail.com>
2017-06-01 17:40:34 +0100

    Update get_faq_item.js

--- js/get_faq_item.js
+++ js/get_faq_item.js
@@ -1,3 +1,11 @@
+// function getItem(title)
+// ----
+// Arguments:
+// [1]. 'title': The title, in plain text, of the faq item you want to access.
+// ----
+// Return:
+// Always attempts to return a jquery object representing the row immediately after the row containing the title (the content of the item).
+
 function getItem(title)
 {
 	var itemItself;
