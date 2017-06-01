898582c2a504b1ec3829e8e3f1db67edbace8d60
Novaras <toribashfear@gmail.com>
2017-06-01 15:51:58 +0100

    Update get_faq_item.js

--- js/get_faq_item.js
+++ js/get_faq_item.js
@@ -2,13 +2,13 @@ function getItem(title)
 {
 	alert("getItem");
 	var itemItself;
-	$(document).ready(function() {
-		$('.tcat').each(function() {
-			if($(this).text() == title)
-			{
-				itemItself = $(this).siblings('.alt1');
-			}
-		});
+	
+	$('.tcat').each(function() {
+		if($(this).text() == title)
+		{
+			itemItself = $(this).siblings('.alt1');
+		}
 	});
+
 	return itemItself;
 }
