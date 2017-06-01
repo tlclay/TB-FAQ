32641e404380f352d5a93379dfb0bead2bafbec6
Novaras <toribashfear@gmail.com>
2017-06-01 16:57:51 +0100

    Update

--- js/staff_pages_linker.js
+++ js/staff_pages_linker.js
@@ -1,6 +1,6 @@
 function staffLinkAll(item)
 {
-	item.find('a.faq_clanSquad').attr("href", "http://forum.toribash.com/faq.php?faq=clans_main#faq_clans_staff"));
+	item.find('a.faq_clanSquad').attr("href", "http://forum.toribash.com/faq.php?faq=clans_main#faq_clans_staff");
 }
 
 function staffLinkThis(item, text)
