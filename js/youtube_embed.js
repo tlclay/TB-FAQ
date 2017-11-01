4a9438cd82a301dcd95ea5f4aeb70278b763f1d4
Novaras <toribashfear@gmail.com>
2017-11-01 17:12:47 +0000

    Create youtube_embed.js

---
+++ js/youtube_embed.js
@@ -0,0 +1,16 @@
+function embedAllYouTube(item)
+{
+	item.children(".youtube").each(function() {
+			var url;
+			var exp = /=(\S+)/;
+			var linkText = $(this).text();
+			
+			var videoID = exp.exec(linkText);
+			if(videoID != null)
+			{
+				url = "https://www.youtube.com/embed/" + videoID[1];
+			}
+			alert(url);
+			$(this).replaceWith('<iframe width="560" height="315" src="' + url + '" frameborder="0" allowfullscreen=""></iframe>');
+	});
+}
