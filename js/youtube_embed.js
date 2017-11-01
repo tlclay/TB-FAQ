1e8c49f94a888accdea36f5c24232a4a3163ace3
Novaras <toribashfear@gmail.com>
2017-11-01 18:57:36 +0000

    Update youtube_embed.js

--- js/youtube_embed.js
+++ js/youtube_embed.js
@@ -1,8 +1,6 @@
 function embedAllYouTube(item)
 {
-	alert("item: " + item.text());
 	item.find(".youtube").each(function() {
-			alert("youtube detected: " + $(this).text());
 			var url;
 			var exp = /=(\S+)/;
 			var linkText = $(this).text();
@@ -12,7 +10,6 @@ function embedAllYouTube(item)
 			{
 				url = "https://www.youtube.com/embed/" + videoID[1];
 			}
-			alert(url);
 			$(this).replaceWith('<iframe width="560" height="315" src="' + url + '" frameborder="0" allowfullscreen=""></iframe>');
 	});
 }
