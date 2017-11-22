594680199523e3ae1e610e8d5ea6fe451bf05771
Novaras <toribashfear@gmail.com>
2017-11-22 04:54:54 +0000

    Update spoiler.js

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,27 +1,22 @@
-// Toggle contents display: .slideDown() / .slideUp() + some css changes.
-function toggleSpoilerState(spoiler) {
-    var spoilerHeader = $(spoiler).find('.spoiler_header_bbc:first');
-    var spoilerContent = $(spoiler).find('.spoiler_content_bbc:first');
-    var spoilerButton = $(spoiler).find('.spoiler_button_bbc:first');
-    var spoilerHR = $(spoiler).find('.spoiler_hr_bbc:first');
-
-    if (spoilerContent.is(':hidden')) {
-        spoilerHeader.css('padding-bottom', '0');
-        spoilerContent.slideDown(75);
-        spoilerButton.text('Hide');
-        spoilerHR.show();
-    } else {
-        spoilerHeader.css('padding-bottom', '0.2rem');
-        spoilerContent.slideUp(75);
-        spoilerButton.text('Show');
-        spoilerHR.hide();
-    }
-}
-
-// Click listener
+// Listens for click event on .spoiler_button_bbc, then toggles css.
 $(document).ready(function() {
     $('body').on('click', '.spoiler_button_bbc', function(e) {
         var spoiler = $(e.target).closest('.spoiler_bbc');
-        toggleSpoilerState(spoiler);
+        var spoilerHeader = $(spoiler).find('.spoiler_header_bbc:first');
+        var spoilerContent = $(spoiler).find('.spoiler_content_bbc:first');
+        var spoilerButton = $(spoiler).find('.spoiler_button_bbc:first');
+        var spoilerHR = $(spoiler).find('.spoiler_hr_bbc:first');
+
+        if (spoilerContent.is(':hidden')) {
+            spoilerHeader.css('padding-bottom', '0');
+            spoilerContent.slideDown(75);
+            spoilerButton.text('Hide');
+            spoilerHR.show();
+        } else {
+            spoilerHeader.css('padding-bottom', '0.2rem');
+            spoilerContent.slideUp(75);
+            spoilerButton.text('Show');
+            spoilerHR.hide();
+        }
     });
 });
