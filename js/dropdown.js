68b3c4fa1c4940120cde9cab24c5c0f1f173a29c
Novaras <toribashfear@gmail.com>
2017-11-22 06:15:33 +0000

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	$('.drop_arrow_bbc').html(' ►');
-	$('.drop_arrow_bbc').css('color', 'black');
+    $('.drop_arrow_bbc').html(' ►');
+    $('.drop_arrow_bbc').css('color', 'black');
 
     $('body').on('click', '.drop_title_bbc', function(e) {
         var dropdown = $(e.target).closest('.dropdown_bbc');
