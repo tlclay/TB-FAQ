671e1a7aad1235e9f846fa82c82a236871508964
Novaras <toribashfear@gmail.com>
2017-11-22 05:45:08 +0000

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,44 +1,15 @@
-function hideDropContent(content, arrow) {
-    content.attr('hidden', 'hidden');
-    arrow.html('►');
-}
+$(document).ready(function() {
+    $('body').on('click', '.drop_title_bbc', function(e) {
+        var dropdown = $(e.target).closest('.dropdown_bbc');
+        var dropArrow = $(dropdown).find('.drop_arrow_bbc:first');
+        var dropContent = $(dropdown).find('.drop_content_bbc:first');
 
-function showDropContent(content, arrow) {
-    content.removeAttr('hidden');
-    content.css('border-left', '1px dashed black');
-    content.children().each(function() {
-        if (!$(this).hasClass('drop_content_item_bbc')) {
-            $(this).show();
+        if (dropContent.is(':hidden')) {
+            dropArrow.html(' ▼');
+            dropContent.slideDown(75);
+        } else {
+            dropArrow.html(' ►');
+            dropContent.slideUp(75);
         }
     });
-    content.contents().filter(function() {
-        return (this.nodeType == 3);
-    }).remove();
-    arrow.html('▼');
-}
-
-$(document).ready(function() {
-	$('strong.drop_title_bbc').on("click", function(e) {
-		var dropdown = $(e.target).closest('div.dropdown_bbc');
-		var title = $(dropdown).children('strong.drop_title_bbc');
-		var content = $(dropdown).children('ul.drop_content_bbc');
-		var arrow = $(title).children('span.drop_arrow_bbc');
-
-		//alert("t: " + title.html() + "\nc: " + content.html() + "\na: " + arrow.html());
-		if (content.attr('hidden') !== undefined) {
-			showDropContent(content, arrow);
-		} else {
-			hideDropContent(content, arrow);
-		}
-	});
-});
-
-$(document).ready(function() {
-    var content = $('div.dropdown_bbc').children('ul.drop_content_bbc');
-    var title = $('div.dropdown_bbc').children('strong.drop_title_bbc').css({
-        'cursor': 'pointer',
-        'color': '#9f0000'
-    });
-    var arrow = $('div.dropdown_bbc').children('strong.drop_title_bbc').children('span.drop_arrow_bbc');
-    hideDropContent(content, arrow);
 });
